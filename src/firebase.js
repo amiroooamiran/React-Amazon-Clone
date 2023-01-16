815caf1ec63e327275caf48b60705d89dce874a5
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-01-17 02:04:57 +0330

    Basic amazon Clone Config
    In this Commit I create the Basic Project For amazon Clone In React, Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBpif1ww-mMzHnzgnpp0POTWlA1Msl5Z2U",
+    authDomain: "cloan-85e0b.firebaseapp.com",
+    projectId: "cloan-85e0b",
+    storageBucket: "cloan-85e0b.appspot.com",
+    messagingSenderId: "287320709244",
+    appId: "1:287320709244:web:afe1a3c00e03f01f30d1f9",
+    measurementId: "G-4HE6KF08JZ"
+};
