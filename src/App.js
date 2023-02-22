4b4f22ff419619a92b0bde9ab9654261fb37ff03
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-02-22 19:35:57 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import './App.css';
+import React, {useState} from "react";
 
+// importing components
 import Header from './header/Header';
+import Home from './home/Home';
+import Footer from "./footer/Footer";
+
+import Chackeout from "./chakeout/Chackeout";
+// import styling
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login/Login";
 
 function App() {
   return (
-    <div>
-       {/* Header Component */}
-       <Header />
-    </div>
+    <Router>
+      <div>
+      
+      <Routes>
+
+          {/* this section for Home Page */}
+          <Route path="/" element={<><Header /><Home /><Footer /></>} />
+          {/* this Section  For ChakeOut  */}
+          <Route path="Checkout" element={<><Header /><Chackeout /><Footer /></>} />
+
+          {/* this Section  for Login  Page  */}
+          <Route path="login" element={<><Login /></>} />
+          
+      </Routes>
+      
+      </div>
+    </Router>
   );
 }
 
