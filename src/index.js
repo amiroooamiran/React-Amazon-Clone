bab1411d98b83ebffbb9898d9ddc05d591adac31
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-02-22 19:37:25 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateContext, StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
+    
   </React.StrictMode>
 );
 
